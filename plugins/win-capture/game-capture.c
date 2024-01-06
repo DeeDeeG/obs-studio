4835342e4ed532b9913778f8dd9ab78aaf4fd792
DeeDeeG <DeeDeeG@users.noreply.github.com>
2024-01-06 18:52:33 -0500

    Revert "win-capture: Always have some capture FPS limit"
    This reverts commit ec5059cee1d7f140e942bf7ad45645b23dc31dc3.

--- plugins/win-capture/game-capture.c
+++ plugins/win-capture/game-capture.c
@@ -823,17 +823,9 @@ static inline void reset_frame_interval(struct game_capture *gc)
 	struct obs_video_info ovi;
 	uint64_t interval = 0;
 
-	if (obs_get_video_info(&ovi)) {
+	if (gc->config.limit_framerate && obs_get_video_info(&ovi)) {
 		interval =
 			util_mul_div64(ovi.fps_den, 1000000000ULL, ovi.fps_num);
-
-		/* Always limit capture framerate to some extent.  If a game
-		 * running at 900 FPS is being captured without some sort of
-		 * limited capture interval, it will dramatically reduce
-		 * performance. */
-		if (!gc->config.limit_framerate)
-			interval /= 2;
-	}
 
 	gc->global_hook_info->frame_interval = interval;
 }
